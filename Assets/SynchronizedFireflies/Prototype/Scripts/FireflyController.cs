6fb8fd7e3874cb344be987a34adb943a05303cf3
yuki_ito <y.itou.physics@gmail.com>
2016-06-09 00:47:20 +0900

    プロトタイプを作った

---
+++ Assets/SynchronizedFireflies/Prototype/Scripts/FireflyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireflyController : MonoBehaviour {
+    Material m_material;
+
+    public float naturalAngularFreq;     // 個体固有の角速度
+    public float phase;             // 明滅の位相
+    public float angularFreq;       // 位相の変化速度
+
+	void Start ()
+    {
+        m_material = GetComponent<MeshRenderer>().material;
+        naturalAngularFreq = 2.0f * Mathf.PI * (0.3f + Random.Range(-0.1f, 0.1f));
+        phase = 2.0f * Mathf.PI * Random.Range(0.0f, 1.0f);
+
+        Camera camera = Camera.main;
+        float x = camera.aspect * Random.Range(-camera.orthographicSize, camera.orthographicSize);
+        float y = Random.Range(-camera.orthographicSize, camera.orthographicSize);
+        transform.position = new Vector3(x, y, 0.0f);
+	}
+
+    void Update()
+    {
+        float intensity = 0.5f + 0.5f * Mathf.Sin(phase);
+        m_material.color = Color.white * intensity;
+    }
+
+    public void UpdatePhase()
+    {
+        phase += angularFreq * Time.deltaTime;
+    }
+}
---
+++ Assets/SynchronizedFireflies/Prototype/Scripts/FireflyManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireflyManager : MonoBehaviour
+{
+    #region variables
+    public GameObject fireflies;
+
+    static FireflyManager m_instance;
+    public FireflyManager Instance
+    {
+        get {
+            if (m_instance == null)
+            {
+                m_instance = new FireflyManager();
+            }
+            return m_instance;
+        }   
+    }
+
+    FireflyController[] m_controllers;
+    public float couplingConst = 1.0f;  // 結合強度
+    #endregion
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        m_controllers = fireflies.GetComponentsInChildren<FireflyController>();
+    }
+
+	void Update ()
+    {
+	    for (int i = 0; i < m_controllers.Length; i++)
+        {
+            float angularVelocity = m_controllers[i].naturalAngularFreq;
+            for (int j = 0; j < i; j++)
+            {
+                float w_ij = couplingConst / (1.0f + Vector3.SqrMagnitude(
+                    m_controllers[j].transform.position - m_controllers[i].transform.position
+                ));
+                angularVelocity += w_ij * Mathf.Sin(
+                        m_controllers[j].phase - m_controllers[i].phase
+                    );
+            }
+            m_controllers[i].angularFreq = angularVelocity;
+        }
+
+        for (int i = 0; i < m_controllers.Length; i++)
+        {
+            m_controllers[i].UpdatePhase();
+        }
+	}
+
+    void Sync()
+    {
+
+    }
+}
